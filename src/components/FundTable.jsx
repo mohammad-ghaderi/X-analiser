232967276341502672fbdcaf4f52bdd349ba39fa
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-03-01 00:32:29 +0330

    sentiment section has been added

--- src/components/FundTable.jsx
+++ src/components/FundTable.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { Table, Form, Button, InputGroup } from 'react-bootstrap';
-import { FUNDAMENTAL_ANALYSIS_TYPE1 } from '../designs/Tables';
+import { FUNDAMENTAL_ANALYSIS_TYPE1, FUNDAMENTAL_ANALYSIS_TYPE2 } from '../designs/Tables';
 import '../styles/table.css';
 
+const symbols = ['fa-greater-than', 'fa-less-than', 'fa-equals', ''];
+const values = ['GBP', 'USD', 'EUR', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'SEK', 'NZD', 'MXN', 'SGD', 'HKD', 'NOK', 'KRW']
+
+
+
 const FundTable = ({ type }) => {
+    if (type === 1) return <FundTable2/>
+    return <FundTable1/>
+}
 
-    const values = ['GBP', 'USD', 'EUR', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'SEK', 'NZD', 'MXN', 'SGD', 'HKD', 'NOK', 'KRW']
 
+const FundTable1 = () => {
     const [firstIdx, setFirstIdx] = useState(0);
     const [secondIdx, setSecondIdx] = useState(1);
     const [data, setData] = useState({});
-
-    const symbols = ['fa-greater-than', 'fa-less-than', 'fa-equals', ''];
 
     const dataHandler = (index, idx, value) => {        
         let newData = {...data};
@@ -131,5 +137,89 @@ const FundTable = ({ type }) => {
         </Table>
     )
 }
+
+const FundTable2 = () => {
+    const [data, setData] = useState({});
+
+    const dataHandler = (index, idx, value) => {        
+        let newData = {...data};
+        let line = newData[index] === undefined ? {} : newData[index];
+        line[idx] = value;
+        console.log(line);
+        newData[index] = line;
+        setData(newData);
+        console.log(newData);
+    }
+
+    const compareHandler = (index) => {
+        let newCompare = (data[index] !== undefined && data[index][2] !== undefined) ? data[index][2] : -1;
+        newCompare++;
+        newCompare %= 3;        
+        dataHandler(index, 2, newCompare);
+    }
+    
+    return (
+        <Table bordered className={'fundamental-analysis fund-analysis2'} variant='secondary'>
+            <thead>
+                <tr>
+                    <th className='p-3' style={{width : '70px'}}>Data</th>
+                    <th className='p-3'>Currency</th>
+                    <th className='p-3' colSpan={9}>USD</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {FUNDAMENTAL_ANALYSIS_TYPE2.body.map((tr, index) => (
+                    <tr key={index}>
+                        {tr.map((td, idx) => (
+                            <td className={td.class?td.class:''}
+                                colSpan={td.colSpan?td.colSpan:'1'}
+                                rowSpan={td.rowSpan?td.rowSpan:'1'}
+                                key={idx}
+                                style={td.style&&td.style}
+                            >
+                                {td.content && td.content}
+                                {td.type === 'cmp' && 
+                                    <Button variant={`${data[index] !== undefined && data[index][2] !== undefined ? 'outline-': ''}secondary`}
+                                            className='p-2 border-0 border-0' 
+                                            style={{height: '100%', width: '100%', position: 'absolute', top: 0, left: 0, fontSize: '1.1rem'}}
+                                            onClick={() => compareHandler(index)}
+                                    >   
+                                        <i className={`fa-solid ${symbols[((data[index] !== undefined && data[index][2] !== undefined) ? data[index][2] : 3)]}`}></i>
+                                    </Button>
+                                }
+                                {td.type === 'inp' && 
+                                    <InputGroup className="" style={{position: 'absolute', height: '100%', top: '0', left: '0'}}>
+                                        <Form.Control
+                                        aria-label="Small"
+                                        aria-describedby="inputGroup-sizing-lg"
+                                        style={{height: '100%'}}
+                                        onChange={(e) => dataHandler(index, idx, e.target.value)}
+                                        value={(data[index] !== undefined && data[index][idx] !== undefined) ? data[index][idx] : ''}
+                                        />
+                                    </InputGroup>
+                                }
+                                {td.type === 'inp-textarea' && 
+                                    <InputGroup className="" style={{ position: 'absolute', left: '0', top: '0', height: '100%'}}>
+                                        <Form.Control
+                                        as="textarea"
+                                        aria-label="Small"
+                                        aria-describedby="inputGroup-sizing-lg"
+                                        style={{height: '100%', resize: 'none'}}
+                                        onChange={(e) => dataHandler(index, idx, e.target.value)}
+                                        value={(data[index] !== undefined && data[index][idx] !== undefined) ? data[index][idx] : ''}
+                                        />
+                                    </InputGroup>
+                                }
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+
+        </Table>
+    )
+}
+
 
 export default FundTable
--- src/components/SentimentAnalisis.jsx
+++ src/components/SentimentAnalisis.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {Col} from 'react-bootstrap';
+import SelectionLRS from './SelectionLRS';
+
+import { SENTIMENT_ANALYSIS} from '../designs/Tables';
 
 const SentimentAnalisis = () => {
   return (
-    <div>SentimentAnalisis</div>
+    <Col xs={6} className='p-3'>
+        <SelectionLRS template={SENTIMENT_ANALYSIS}/> 
+      </Col>
   )
 }
 
