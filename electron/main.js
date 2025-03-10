836a68f52160ae71193ca4e37f10ade0ff82d613
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-03-10 10:03:42 +0330

    adding logo

--- electron/main.js
+++ electron/main.js
@@ -11,6 +11,7 @@ const createWindow = () => {
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
+            icon: path.join(__dirname, "public", "logo.svg"), // Set icon
             nodeIntegration: true,
             contextIsolation: true,
         },
